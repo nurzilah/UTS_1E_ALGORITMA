a9f22cdd7349a70d3ac2fce89869d786f75937d8
ASUS <nurzila2944@gmail.com>
2022-11-14 12:19:23 +0700

    ok

--- src/Jawaban_no_3.java
+++ src/Jawaban_no_3.java
@@ -1,5 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Jawaban_no_3 {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        String[]InfoBus=new String[]{"01-TopLevel","02-MiddleLevel","03-RegularStaff","04-Staff"};
+        int i;
+
+        System.out.println("======================================");
+        System.out.println("========== Kamif From Cinde ==========");
+
+        System.out.println("1. Bus Top Level Untuk C ");
+        System.out.println("1. Bus Middle Level Untuk J");
+        System.out.println("1. Bus Regullar Staff Untuk N");
+        System.out.println("1. Bus Staff Untuk P");
+        System.out.println();
+        System.out.println("Masukan No NIK Kariawan");
+        Scanner satu = new Scanner(System.in);
+        i = satu.nextInt();
+
+        System.out.println("Anda Di Tempatkan Pada Bus Ke" + i);
+        System.out.println();
+        System.out.println("======================================");
+        System.out.println("Jumlah Bus Top Level " + i  );
+        System.out.println("Jumlah Bus Middle Level " + i);
+        System.out.println("Jumlah Bus Regullar Staff " + i);
+        System.out.println("Jumlah Bus Staff " + i);
+        System.out.println("======================================");
+
+
+
+
     }
+
 }
